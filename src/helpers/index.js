fdffcaa11cdfc4ae6b6d1e674eaebd8df2f47548
ksan199 <techno199@live.ru>
2019-04-18 23:17:41 +0300

    update consts

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,26 @@
 export const ItemTypes = {
-  BOX: 'BOX'
+  BOX: 'BOX',
+  TAG: 'TAG'
 }
+
+export const TagTypes = {
+  NW: 'NW',
+  N: 'N',
+  NE: 'NE',
+  W: 'W',
+  E: 'E',
+  SW: 'SW',
+  S: 'S',
+  SE: 'SE'
+}
+
+/** How many height's can maximum width contain */
+export const MAX_RESIZE_RATIO = 1
+/** How many height's can minimum width contain */
+export const MIN_RESIZE_RATIO = 1/1.5
+/** Maximum image markup width or height */
+export const MAX_IMAGE_DIMENSION_LENGHT = 604
+/** Minimum image markup width or height */
+export const MIN_IMAGE_DIMENSION_LENGHT = 200
+/** Minimum crop area width or height */
+export const MIN_IMAGE_CROP_DIMENSION_LENGHT = 100
